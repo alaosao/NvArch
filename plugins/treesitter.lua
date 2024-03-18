485095e8f5d48bb5b0210a4b8b94900152d53677
mitzsou <145884925+mitzsou@users.noreply.github.com>
2024-03-17 20:51:40 -0400

    Updated

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -10,5 +10,11 @@ return {
         indent = { enable = true },
       })
     end
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
 }
